762902c4537ea93fbf7ca801ed9185198336b97a
r-werner <rwerner@gmx.de>
2024-07-14 14:49:06 +0200

    fixed notebook support

---
+++ examples/states-langium/interpreter/cli.ts
@@ -0,0 +1,26 @@
+import { Command } from 'commander';
+import fs from 'fs';
+import { runInterpreter } from './runner.js';
+// import pkg from '../../package.json' with { type: 'json' };
+
+const program = new Command();
+
+program
+    // .version(pkg.version);
+    .version('0.0.2');
+
+program
+    .command('run')
+    .argument('<file>')
+    .action(runCommand);
+
+program.parse(process.argv);
+
+async function runCommand(file: string): Promise<void> {
+    const now = Date.now();
+    const content = await fs.promises.readFile(file, 'utf-8');
+    await runInterpreter(content, {
+        log: value => console.log(`${value}`)
+    });
+    console.log(`Lox program finished running in ${Date.now() - now}ms`);
+}
